afb0609536bb5517433deeb9bdf96c1dda480e51
hg <bhg5460@gmail.com>
2023-03-20 00:49:11 +0900

    fix: 파라미터 바인딩 validation 동작하지 않는 이슈 수정
    블로그 검색모델에 @Valid 어노테이션 추가
    
    BindingException 처리 메소드 추가
    
    Signed-off-by: hg <bhg5460@gmail.com>

--- src/main/java/com/hgbong/blogsearch/exception/ExceptionResponse.java
+++ src/main/java/com/hgbong/blogsearch/exception/ExceptionResponse.java
@@ -1,14 +1,19 @@
 package com.hgbong.blogsearch.exception;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ExceptionResponse {
 
     private final HttpStatus status;
@@ -16,9 +21,18 @@ public class ExceptionResponse {
 
     private LocalDateTime timestamp;
 
+    private List<Field> fields;
+
     public ExceptionResponse(HttpStatus status, String message) {
         this.status = status;
         this.message = message;
         this.timestamp = LocalDateTime.now();
+    }
+
+    @Getter
+    @Setter
+    static class Field {
+        private String fieldName;
+        private String message;
     }
 }
