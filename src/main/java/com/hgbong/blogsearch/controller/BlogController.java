2b1d3ecf2854901522c5451ed0408e1b4d5f3068
hg <bhg5460@gmail.com>
2023-03-21 23:09:00 +0900

    improve: 스케줄러를 사용한 검색어 저장 방식으로 변경
    AS-IS 검색어가 입력될 때마다 DB 작업
    TO-BE 검색어와 횟수를 캐싱하고 주기적으로 DB 작업
    
    (DB 비동기 작업 시 오류가 발생한 경우, throw는 대신 warning level logging)
    
    FIXME
    - 스케줄러 사용 시 별도 tx 생성으로 인한 TC 오류 이슈
    
    Signed-off-by: hg <bhg5460@gmail.com>

--- src/main/java/com/hgbong/blogsearch/controller/BlogController.java
+++ src/main/java/com/hgbong/blogsearch/controller/BlogController.java
@@ -5,6 +5,7 @@ import com.hgbong.blogsearch.model.search.BlogSearchCriteria;
 import com.hgbong.blogsearch.model.search.DocumentDto;
 import com.hgbong.blogsearch.service.BlogSearchService;
 import com.hgbong.blogsearch.service.QueryService;
+import com.hgbong.blogsearch.util.QueryStorage;
 import io.swagger.annotations.ApiOperation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -12,8 +13,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @RestController
@@ -23,20 +22,14 @@ public class BlogController {
 
     private final BlogSearchService blogSearchService;
 
-    private final QueryService queryService;
+    private final QueryStorage queryStorage;
 
     @ApiOperation(value = "블로그 검색", notes = "키워드를 입력하여 블로그를 페이지 구조로 조회할 수 있다.\n" +
         "sort 기능을 사용해 정확도(accuracy)순 또는 최신순(recency)으로 정렬하여 조회 가능하다. (기본정렬: accuracy)")
     @GetMapping
     public PageResponse<DocumentDto> pageBlogs(@Valid BlogSearchCriteria blogSearch) {
 
-        // todo 검색 기능과 분리 - insert 실패해도 API 동작 , 조회 성능문제 되지 않도록 처리 등
-
-        CompletableFuture<Void> queryCountAsync = queryService.increaseQueryCountAsync(blogSearch.getQuery());
-        queryCountAsync.exceptionally(throwable -> {
-            log.warn("insert or update query has problem : ", throwable);
-            return null;
-        });
+        queryStorage.querySearched(blogSearch.getQuery());
 
         return blogSearchService.pageBlogs(blogSearch);
     }
--- src/main/java/com/hgbong/blogsearch/util/QueryStorage.java
+++ src/main/java/com/hgbong/blogsearch/util/QueryStorage.java
@@ -1,2 +1,47 @@
-package com.hgbong.blogsearch.util;public class QueryStorage {
+package com.hgbong.blogsearch.util;
+
+import com.hgbong.blogsearch.service.QueryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class QueryStorage {
+
+    // 정해진 시간동안 검색된 검색어와 횟수를 저장하는 Map
+    private final ConcurrentHashMap<String, Long> queryMap = new ConcurrentHashMap();
+
+    private final QueryService queryService;
+
+    /**
+     * 특정 검색어가 검색된 경우 해당 검색어 카운트를 1 증가한다.
+     *
+     * @param query 검색어
+     */
+    public void querySearched(String query) {
+        queryMap.put(query, queryMap.getOrDefault(query, 0l) + 1);
+    }
+
+    /**
+     * 정해진 시간마다, 저장된 검색어 카운트를 DB에 저장한다.
+     */
+    @Scheduled(cron = "${query-cron:*/1 * * * * *}")
+    public void saveQueryCount() {
+        Set<Map.Entry<String, Long>> entries = queryMap.entrySet();
+        for (Map.Entry<String, Long> entry : entries) {
+            queryService.addQueryCount(entry.getKey(), entry.getValue())
+                .exceptionally(throwable -> {
+                    log.warn("insert or update query has problem : ", throwable);
+                    return null;
+                });
+            queryMap.remove(entry.getKey());
+        }
+    }
 }
