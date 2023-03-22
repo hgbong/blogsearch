8e017fa7c7ae0b835a2d450029c3a34529753f06
hg <bhg5460@gmail.com>
2023-03-22 21:05:47 +0900

    fix: 테스트 케이스 실패 케이스 조치
    AS-IS 문제점
    - 검색어 저장이 스케줄러를 통해 입력되고 롤백되지 않음
    - 이로 인해 QueryControllerTest에서 예측하지 못한 TC 실패 발생
    
    TO-BE
    - profile=test 실행 시 스케줄러 동작하지 않게 수정 (app-test.yml)
    - 대신 검색어 저장을 queryService 로부터 실행
    - (QueryController 기능 -> 저장된 검색어를 개수별로 조회할 수 있는가)
    
    Signed-off-by: hg <bhg5460@gmail.com>

--- src/test/java/com/hgbong/blogsearch/controller/QueryControllerTest.java
+++ src/test/java/com/hgbong/blogsearch/controller/QueryControllerTest.java
@@ -2,30 +2,18 @@ package com.hgbong.blogsearch.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.hgbong.blogsearch.entity.Query;
-import com.hgbong.blogsearch.repository.QueryRepository;
-import com.hgbong.blogsearch.util.QueryStorage;
+import com.hgbong.blogsearch.service.QueryService;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.Duration;
-import java.util.Date;
-import java.util.List;
-
-import static org.awaitility.Awaitility.await;
-import static org.mockito.Mockito.atLeast;
-import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -35,18 +23,9 @@ class QueryControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @SpyBean
-    private QueryStorage queryStorage;
-
     @Autowired
-    private QueryRepository queryRepository;
+    private QueryService queryService;
 
-
-    // fixme  해당 내용 수정시까지 테스트 보류!!  - 스케줄러 관련 테스트 작성
-    //  AS-IS Async job을 profile="test" 에서 동작하지 않도록 수정 -> Async job을 main 쓰레드에서 실행 -> rollback O
-    //  TO-BE scheduler 에서 주기적으로 DB 요청 -> 별도 쓰레드에서 tx 관리 -> rollback X
-
-    /*
     @Test
     void listFavoriteQueries_queryNotExists() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/querys/favorite"))
@@ -55,18 +34,16 @@ class QueryControllerTest {
     }
 
     @Test
+    @DisplayName("i (i<10)개의 검색어에 대해, 각각 i번 호출하고 결과의 처음과 마지막 원소 확인")
     void listFavoriteQueries_queryLessThan10() throws Exception {
-        for (int i = 0; i < 3; i++) {
-            mockMvc.perform(MockMvcRequestBuilders.get("/blogs")
-                .queryParam("query","test" + i));
+        int queryCnt = 3;
+        for (int i = 1; i <= queryCnt; i++) {
+            queryService.addQueryCount("test"+i, i);
         }
-
-        await().atMost(Duration.ofSeconds(30))
-                .untilAsserted(() -> verify(queryStorage, atLeast(2)).saveQueryCount());
 
         mockMvc.perform(MockMvcRequestBuilders.get("/querys/favorite"))
             .andExpect(MockMvcResultMatchers.status().isOk())
-            .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(3)));
+            .andExpect(MockMvcResultMatchers.jsonPath("$", Matchers.hasSize(queryCnt)));
     }
 
     @Test
@@ -74,13 +51,8 @@ class QueryControllerTest {
     void listFavoriteQueries_queryMoreThan10() throws Exception {
         int queryCnt = 11;
         for (int i = 1; i <= queryCnt; i++) {
-            for (int j = 0; j < i; j++) {
-                mockMvc.perform(MockMvcRequestBuilders.get("/blogs")
-                    .queryParam("query","test" + i));
-            }
+            queryService.addQueryCount("test"+i, i);
         }
-
-        // fixme  위의 TC와 동일한 이슈
 
         String response = mockMvc.perform(MockMvcRequestBuilders.get("/querys/favorite"))
             .andExpect(MockMvcResultMatchers.status().isOk())
@@ -96,5 +68,4 @@ class QueryControllerTest {
         Assertions.assertEquals(last.path("query").asText(), "test" + (queryCnt - 9));
         Assertions.assertEquals(last.path("count").numberValue(), queryCnt - 9);
     }
-    */
 }
