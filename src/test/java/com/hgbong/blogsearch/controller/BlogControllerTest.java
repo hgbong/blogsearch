63e9b1bec487ac440a16e873d430f43cfa3153b4
hg <bhg5460@gmail.com>
2023-03-22 21:05:47 +0900

    fix: 테스트 케이스 실패 케이스 조치
    AS-IS 문제점
    - 검색어 저장이 스케줄러를 통해 입력되고 롤백되지 않음
    - 이로 인해 QueryControllerTest에서 예측하지 못한 TC 실패 발생
    
    TO-BE
    - profile=test 실행 시 스케줄러 동작하지 않게 수정 (app-test.yml)
    - 대신 검색어 저장을 queryService 로부터 실행
    - (QueryController 기능 -> 저장된 검색어를 개수별로 조회할 수 있는가)
    
    Signed-off-by: hg <bhg5460@gmail.com>

--- src/test/java/com/hgbong/blogsearch/controller/BlogControllerTest.java
+++ src/test/java/com/hgbong/blogsearch/controller/BlogControllerTest.java
@@ -2,16 +2,12 @@ package com.hgbong.blogsearch.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.concurrent.Executor;
 
 @SpringBootTest
 @AutoConfigureMockMvc
